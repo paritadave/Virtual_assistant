55153faf8b9cbb609a472e02282782894d80819e
Parita Dave <72275690+paritadave@users.noreply.github.com>
2026-01-20 02:13:50 -0500

    Change initial sidebar state to collapsed

--- VA_v1.py
+++ VA_v1.py
@@ -20,11 +20,13 @@ from tabs.tab_edit import show_edit_tab
 from tabs.tab_about import show_about_tab
 
 # ---------- Streamlit Page Setup ----------
+import streamlit as st
+
 st.set_page_config(
     page_title="AI Virtual Assistant",
     layout="centered",
     page_icon="🤖",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 # ==========================================================
